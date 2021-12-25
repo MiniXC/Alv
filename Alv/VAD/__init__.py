f269c349dc8c1e7f34c356074bccc5117b8a6cbb
Christoph Minixhofer <christoph.minixhofer@gmail.com>
2021-12-25 21:13:13 +0000

    add segmention barebones

--- Alv/VAD/__init__.py
+++ Alv/VAD/__init__.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 
 
 class VAD(ABC):
-    def __init__(self, hyper_parameters):
+    def __init__(self, hyper_parameters, boundary_treshhold_in_ms=100, padding_in_ms=20):
         self.init_hyper_parameters(hyper_parameters)
 
     @abstractmethod
@@ -13,3 +13,9 @@ class VAD(ABC):
     @abstractmethod
     def detect_activity(self, audio) -> List[Tuple[int, int]]:
         return NotImplemented
+
+    def segment(self, recorder):
+        for chunk in recorder.record():
+            for segment in self.detect_activity(chunk):
+                yield NotImplemented
+                # do segmention here next, and yield nicely segmented chunks
